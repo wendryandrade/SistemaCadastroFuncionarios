c753208fac87072efc08568267364ce4a109221e
Wendrya Andrade <wendrya1@gmail.com>
2021-03-06 21:46:34 -0300

    Add files via upload

---
+++ SQLQuery_BDFuncionario.sql
@@ -0,0 +1,10 @@
+create table Funcionario
+(
+  CodFuncionario int identity(1,1) primary key,
+  Nome varchar(400) not null,
+  DataNascimento datetime not null,
+  Salario numeric(18,2) not null,
+  Atividades varchar(MAX) not null
+)
+
+select * from Funcionario
